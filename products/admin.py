b52b479bfdf5fb1b5252bdf05f17d7775ee40441
minaZaeri2002 <minazaeri2002@gmail.com>
2025-02-28 22:27:19 +0330

    Added admin panel and Product model.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Product, ProductImage, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['name', ]
+    fields = ['name', ]
+    search_fields = ['name', ]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+    fields = ['image']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    inlines = [ProductImageInline]
+    list_display = ['title', 'description', 'price', 'quantity', 'category']
+    fields = ['title', 'description', 'price', 'quantity', 'category']
+    search_fields = ['title', 'category']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+
